7c87c8c27e3fb790e57cd050a505ef043776b084
Jose Ortega <link.josef@gmail.com>
2016-02-26 01:05:49 -0500

    Improved view view model viewmodel example

--- mvvm_example/src/main/java/com/globant/android/mvvm_example/MainActivity.java
+++ mvvm_example/src/main/java/com/globant/android/mvvm_example/MainActivity.java
@@ -3,19 +3,16 @@ package com.globant.android.mvvm_example;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.globant.android.mvvm_example.databinding.ActivityMainBinding;
 
-
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-		User user = new User("Test", "User");
-		binding.setUservm(new UserViewModel(user));
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Max", "Power");
+        binding.setUservm(new UserViewModel(user));
+    }
 }
--- mvvm_example/src/main/java/com/globant/android/mvvm_example/UserViewModel.java
+++ mvvm_example/src/main/java/com/globant/android/mvvm_example/UserViewModel.java
@@ -10,49 +10,67 @@ import android.text.TextWatcher;
  */
 public class UserViewModel extends BaseObservable {
 
-	private User user;
+    private User user;
 
-	public UserViewModel(User user) {
-		this.user = user;
-	}
+    public UserViewModel(User user) {
+        this.user = user;
+    }
 
-	@Bindable
-	public String getUserFirstName() {
-		return this.user.getFirstName();
-	}
+    @Bindable
+    public String getUserFirstName() {
+        return this.user.getFirstName();
+    }
 
-	@Bindable
-	public String getUserLastName() {
-		return this.user.getLastName();
-	}
+    @Bindable
+    public String getUserLastName() {
+        return this.user.getLastName();
+    }
 
-	public void setFirstName(String firstName) {
-		this.user.setFirstName(firstName);
-		notifyPropertyChanged(com.globant.android.mvvm_example.BR.userFirstName);
-	}
+    public void setFirstName(String firstName) {
+        this.user.setFirstName(firstName);
+        notifyPropertyChanged(com.globant.android.mvvm_example.BR.userFirstName);
+    }
 
-	public void setLastName(String lastName) {
-		this.user.setLastName(lastName);
-		notifyPropertyChanged(com.globant.android.mvvm_example.BR.userLastName);
-	}
+    public void setLastName(String lastName) {
+        this.user.setLastName(lastName);
+        notifyPropertyChanged(com.globant.android.mvvm_example.BR.userLastName);
+    }
 
+    public TextWatcher getNameWatcher() {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-	public TextWatcher getNameWatcher() {
-		return new TextWatcher() {
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-			}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
 
-			}
+            @Override
+            public void afterTextChanged(Editable s) {
+                setFirstName(s.toString());
+            }
+        };
+    }
 
-			@Override
-			public void afterTextChanged(Editable s) {
-				setFirstName(s.toString());
-			}
-		};
-	}
+    public TextWatcher getLastNameWatcher() {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                setLastName(s.toString());
+            }
+        };
+    }
 }
